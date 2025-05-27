1f768f099fe7e0807862f4a4ef64cba9f004c586
Gabriel Restrepo <gabo1gara@gmail.com>
2025-05-27 12:14:26 -0700

    I add NavBar

--- journal-app/src/journal/layout/JournalLayout.jsx
+++ journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,13 +1,20 @@
 import { Box } from "@mui/system";
+import { NavBar } from "../components/NavBar";
+
+
+const drawerWidth  = 240; 
 
 export const JournalLayout = ({children}) => {
   return (
     <Box sx={{ display: 'flex' }}>
-        {/* Navbar */}
+        
+        
+        <NavBar drawerWidth= { drawerWidth } />
 
         {/* Sidebar */}
 
-        <Box component='main'
+        <Box 
+        component='main'
         sx={{ flexGrow: 1, p:3 }}
         >
             {/* ToolBar */ }
