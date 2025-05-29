480577279fac02143c391e57d9219440b070544d
Gabriel Restrepo <gabo1gara@gmail.com>
2025-05-29 13:08:33 -0700

    I have improve the NoteView version

--- journal-app/src/journal/views/NoteView.jsx
+++ journal-app/src/journal/views/NoteView.jsx
@@ -1,12 +1,49 @@
-import {Grid, Typography } from '@mui/material';
+import { SaveOutlined } from '@mui/icons-material';
+import { Button, Grid, TextField, Typography } from '@mui/material';
 
 export const NoteView = () => {
   return (
-    <Grid container direction='row' justifyContent='space-between' sx={{ mb: 1 }}>
-        <Grid item >
-            <Typography fontSize={ 39 }  fontWeight='light'> Agoust 28,2025</Typography>
-        </Grid>
+    <Grid container direction='column' sx={{ mb: 1 }}>
+      
+      {/* Header: Fecha y Botón */}
+      <Grid
+        container
+        direction='row'
+        justifyContent='space-between'
+        alignItems='center'
+        sx={{ mb: 2 }}
+      >
+        <Typography fontSize={39} fontWeight='light'>
+          August 28, 2025
+        </Typography>
 
+        <Button color='primary' sx={{ padding: 2 }}>
+          <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+          Save
+        </Button>
+      </Grid>
+
+      {/* Campos de texto */}
+      <Grid container direction="column">
+        <TextField
+          type='text'
+          variant='filled'
+          fullWidth
+          placeholder='Enter a title'
+          label='Title'
+          sx={{ border: 'none', mb: 1 }}
+        />
+
+        <TextField
+          type='text'
+          variant='filled'
+          fullWidth
+          multiline
+          placeholder="What's going on today?"
+          label='Description'
+          minRows={5}
+        />
+      </Grid>
     </Grid>
-  )
-}
+  );
+};
