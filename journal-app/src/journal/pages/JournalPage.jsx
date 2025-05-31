714b849b3684214ad79e296a84e21d3e12fdb136
Gabriel Restrepo <gabo1gara@gmail.com>
2025-05-31 11:38:57 -0700

    I added a button on Journal Page

--- journal-app/src/journal/pages/JournalPage.jsx
+++ journal-app/src/journal/pages/JournalPage.jsx
@@ -1,6 +1,7 @@
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NothingSelectedView, NoteView } from "../views";
-
+import { AddOutlined } from "@mui/icons-material";
 
 
 export const JournalPage = () => {
@@ -8,8 +9,23 @@ export const JournalPage = () => {
 
         <JournalLayout>
             {/* <Typography variant='h1' >Lorem ipsum</Typography> */}
-            {/* <NothingSelectedView /> */}
-            <NoteView/>
+         <NothingSelectedView /> 
+        {/* <NoteView/>*/}
+
+        <IconButton
+            size='large'
+            sx={{
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+            position: 'fixed',
+            right: 50,
+            bottom: 50
+            }}
+        >
+            <AddOutlined sx={{ fontSize: 30 }} />
+        </IconButton>
+
         </JournalLayout>
         
     )
